9191b7f63c94325ad52a58478f51e22bf514d927
jim-cassidy <jim-cassidy@outlook.com>
2020-07-07 23:28:48 -0400

    Add files via upload

---
+++ module/forms.py
@@ -0,0 +1,15 @@
+# forms.py
+
+from wtforms import Form, StringField, SelectField, validators
+
+class MusicSearchForm(Form):
+   choices = [('Colleges & Universities', 'Colleges & Universities'),
+              ('Food', 'Food'),
+              ('Events', 'Events')]
+   select = SelectField('Search:', choices=choices)
+   search = StringField('Search ')
+   locationcity = StringField('Location City')
+   locationstate = StringField('Location State')
+   radius = StringField('Radius')
+   limit = StringField('Limit')
+  
